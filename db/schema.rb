4f953b481d7586bec5b145b2671780c1ec430ad2
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-04 12:25:58 -0600

    Remove some commit and recreate recipe and recipr_food model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_163024) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_182223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_163024) do
     t.string "measurement_unit"
     t.integer "price"
     t.string "quantity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.string "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "preparation_time"
+    t.string "cooking_time"
+    t.text "description"
+    t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -29,4 +47,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_163024) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users"
 end
