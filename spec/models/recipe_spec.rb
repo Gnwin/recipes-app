5233e9abbcf9589d3ba18cc611adeb882dcfa8d9
Godwin Nwachukwu <nwgwin@gmail.com>
2022-10-05 22:47:29 +0100

    fix linter errors

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Recipe, type: :model do
   subject do
     Recipe.create(name: 'Apple Pie', user: User.first, preparation_time: 60, cooking_time: 30,
-               description: 'Put Apples and mix them up, cook them for 30 minutes', public: true)
+                  description: 'Put Apples and mix them up, cook them for 30 minutes', public: true)
   end
 
   it 'a new recipe_food is valid' do
