f3d34a060d86ce418b804b1bcde19a79a230d47b
Godwin Nwachukwu <nwgwin@gmail.com>
2022-10-06 10:21:30 +0100

    add template views for food

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
+
+  get "/foods", to: "foods#index"
+  get "/recipes", to: "recipes#index"
+  get "/recipes/:recipe_id", to: "recipes#show"
+  get "/public_recipes", to: "public_recipes#index"
+  get "/shopping_list", to: "shopping_list#index"
 end
