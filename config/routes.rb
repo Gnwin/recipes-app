a7ae70f62a7b4df9a2c617fcba9abeb9864e4cd8
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-07 16:20:31 -0600

    Create the first page before sign up or sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'foods#index'
+  root 'home#index'
 
   get '/recipes', to: 'recipes#index'
   get '/recipes/new', to: 'recipes#new'
